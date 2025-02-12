2f701e4748c5caefe191c901b7a86f0ca8aa5ea9
Javier Jesús Martínez Moreno <jj_dev@outlook.com>
2025-02-11 22:30:09 -0600

    feat: add page's re-rendering function/state

--- part1/reactintro/src/main.jsx
+++ part1/reactintro/src/main.jsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+let counter = 1;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <App counter={counter} />
 )
