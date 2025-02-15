56a00a4417da5e2fc6c13ef94f942083a9d5913b
Javier Jesús Martínez Moreno <jj_dev@outlook.com>
2025-02-14 20:41:59 -0600

    feat: add phone numbers functions for exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from "react"
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]);
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   
   const addName = (event) => {
     event.preventDefault();
     const personObject = { 
-      name: newName, 
+      name: newName,
+      number: newNumber
     }
 
     if (checkDuplicates(newName)) {
@@ -18,11 +20,16 @@ const App = () => {
     } else {
       setPersons(persons.concat(personObject));
       setNewName('');
+      setNewNumber('');
     }
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   }
 
   const checkDuplicates = (personName) => {
@@ -43,14 +50,21 @@ const App = () => {
               onChange={handleNameChange} />
           </div>
           <div>
+            Number: <input type="text" id="personNumber"
+              value={newNumber}
+              onChange={handleNumberChange} />
+          </div>
+          <div>
             <button type="submit">Add</button>
           </div>
         </form>
         <h2>Numbers</h2>
         <ul>
-          {persons.map(person => <li key={person.name}>{person.name}</li>)}
+          {persons.map(person => <li key={person.name}>
+            {person.name} ({person.number})</li>)}
         </ul>
       </div>
+
     </>
   )
 }
