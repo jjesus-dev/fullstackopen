ddc7ff75918c5df5f55387d39f8db29a612f0b47
Javier Jesús Martínez Moreno <jj_dev@outlook.com>
2025-02-14 18:45:25 -0600

    feat: add form controls and state to save new notes

--- part2/reactnotes/src/App.jsx
+++ part2/reactnotes/src/App.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react"
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('a new note...');
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  }
+
   return (
     <>
       <div>
@@ -10,6 +31,12 @@ const App = ({ notes }) => {
             <Note key={note.id} note={note} />
           )}
         </ul>
+        <form onSubmit={addNote}>
+          <input type="text" 
+            value={newNote}
+            onChange={handleNoteChange} />
+          <button type="submit">Save</button>
+        </form>
       </div>
     </>
   )
