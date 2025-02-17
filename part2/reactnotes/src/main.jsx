0e52681040de84c19c2f7dd45f19c92290c375b9
Javier Jesús Martínez Moreno <jj_dev@outlook.com>
2025-02-17 15:53:06 -0600

    feat: add effect to render the notes using axios

--- part2/reactnotes/src/main.jsx
+++ part2/reactnotes/src/main.jsx
@@ -1,15 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import axios from 'axios'
 
 import App from './App.jsx'
 
-axios
-  .get('http://localhost:3001/notes')
-  .then(response => {
-    const notes = response.data;
-    console.log(notes);
-    
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <App notes={notes} />
-    )
-  })
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
