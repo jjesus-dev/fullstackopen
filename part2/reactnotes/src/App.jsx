124b852b387fbf820f51296d86b5fd94caf6af6a
Javier Jesús Martínez Moreno <jj_dev@outlook.com>
2025-02-18 17:40:05 -0600

    refactor: modify app to simplify server interaction

--- part2/reactnotes/src/App.jsx
+++ part2/reactnotes/src/App.jsx
@@ -10,8 +10,8 @@ const App = () => {
 
   const hook = () => {
     noteService.getAll()
-      .then(response => {
-        setNotes(response.data);
+      .then(initialNotes => {
+        setNotes(initialNotes);
       })
   };
   useEffect(hook, []);
@@ -24,8 +24,8 @@ const App = () => {
     }
 
     noteService.create(noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data));
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote));
         setNewNote('');
       })
   }
@@ -43,8 +43,8 @@ const App = () => {
     const changedNote = { ...note, important: !note.important }
 
     noteService.update(id, changedNote)
-      .then(response => {
-        setNotes(notes.map(note => note.id !== id ? note : response.data));
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote));
       })
   }
 
