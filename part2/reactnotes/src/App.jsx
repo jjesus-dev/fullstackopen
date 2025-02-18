50c71afafec6f943a2f5bdbbaa31ec2931e0e1c7
Javier Jesús Martínez Moreno <jj_dev@outlook.com>
2025-02-18 14:02:40 -0600

    refactor: modify app behavior to post notes with axios

--- part2/reactnotes/src/App.jsx
+++ part2/reactnotes/src/App.jsx
@@ -8,11 +8,9 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   const hook = () => {
-    console.log('effect');
     axios
       .get('http://localhost:3001/notes')
       .then(response => {
-        console.log('promise fulfilled');
         setNotes(response.data);
       })
   };
@@ -23,11 +21,15 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1
     }
 
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+      console.log(response);
+      setNotes(notes.concat(response.data));
+      setNewNote('');
+    })
   }
 
   const handleNoteChange = (event) => {
