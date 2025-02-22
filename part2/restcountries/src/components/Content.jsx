81f7e0305876de0dead5eab9761d5ece972b3b85
Javier Jesús Martínez Moreno <jj_dev@outlook.com>
2025-02-21 23:31:09 -0600

    fix: modify countries list rendering options

--- part2/restcountries/src/components/Content.jsx
+++ part2/restcountries/src/components/Content.jsx
@@ -3,7 +3,9 @@ import Country from "./Country"
 const Content = (props) => {
     const count = props.countries.length;
 
-    if (count >= 1 && count <= 10) {
+    if (count === 1) {
+        return null;
+    } else if (count >= 1 && count <= 10) {
         return (    
             <ol>
             {props.countries.map(country => 
