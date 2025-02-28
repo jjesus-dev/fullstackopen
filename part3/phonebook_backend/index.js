be984199be79a4c50ebbbe594d00857d61059ead
Javier J. Martínez M. <jj_dev@outlook.com>
2025-02-27 22:59:06 -0600

    feat: add `POST` custom logging for exercise 3.8

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -26,7 +26,16 @@ let numbers = [
 ];
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('body', (req, res) => {
+    if (req.method === 'POST') {
+        return `- ${JSON.stringify(req.body)}`;
+    } else {
+        return ''
+    }
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get('/', (request, response) => {
     response.send('<p>Phone numbers</p>');
