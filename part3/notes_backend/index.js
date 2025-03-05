4f3b1899661df3ea5993c7c9ddab67162722dae2
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-04 23:23:42 -0600

    refactor: modify `GET` and `POST` to use MongoDB

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -4,23 +4,7 @@ const cors = require('cors');
 const app = express();
 const Note = require('./models/note');
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
+let notes = [];
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method);
@@ -51,41 +35,29 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(note => note.id === id);
-    
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    })
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id)) : 0;
-    
-    return maxId + 1;
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body;
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: "content is missing"
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId()
-    }
+        important: Boolean(body.important) || false
+    })
 
-    notes = notes.concat(note);
+    note.save().then(savedNote => {
+        response.json(savedNote);
+    })
 
-    response.json(note);
 })
 
 app.delete('/api/notes/:id', (request, response) => {
