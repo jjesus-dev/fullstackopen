041291680d4201c56a1f7d7b71521c60e2bdb45c
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-06 14:03:00 -0600

    style: modify files with linting recommendations

--- part3/notes_backend/mongo.js
+++ part3/notes_backend/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-    console.log('give password as argument');
-    process.exit(1);
+  console.log('give password as argument');
+  process.exit(1);
 }
 
 const password = process.argv[2];
@@ -13,16 +13,16 @@ mongoose.set('strictQuery');
 mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean
-})
+  content: String,
+  important: Boolean
+});
 
 const Note = mongoose.model('Note', noteSchema);
 
 const note = new Note({
-    content: 'Test saving notes',
-    important: true
-})
+  content: 'Test saving notes',
+  important: true
+});
 
 // note.save().then(result => {
 //     console.log('note saved!');
@@ -30,8 +30,8 @@ const note = new Note({
 // })
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note);
-    })
-    mongoose.connection.close();
-})
+  result.forEach(note => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
