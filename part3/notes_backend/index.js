1ea8dfde804aa60227dc43d889a10af3f5b1516d
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-05 21:52:31 -0600

    feat: add notes' content validations & restrictions
    refactor: modify `PUT` and `POST` to handle validation errors

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -29,6 +29,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformed id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error);
@@ -58,14 +60,8 @@ app.get('/api/notes/:id', (request, response, next) => {
         })
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body;
-
-    if (body.content === undefined) {
-        return response.status(400).json({
-            error: "content is missing"
-        })
-    }
 
     const note = new Note({
         content: body.content,
@@ -74,7 +70,7 @@ app.post('/api/notes', (request, response) => {
 
     note.save().then(savedNote => {
         response.json(savedNote);
-    })
+    }).catch(error => next(error))
 
 })
 
@@ -88,14 +84,11 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body;
+    const { content, important } = request.body;
 
-    const note = {
-        content: body.content,
-        important: body.important
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(request.params.id,
+            { content, important },
+            { new: true, runValidators: true, context: 'query' })
         .then(updatedNote => {
             response.json(updatedNote);
         }).catch(error => next(error))
