6d39ed7d5ce4eeeac15e3e3711bd0312187a009c
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-05 19:04:50 -0600

    refactor: modify `GET`, `DELETE` & `PUT` for notes
    feat: add error handling middleware

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -24,6 +24,16 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'Unknown Endpoint' });
 }
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformed id' })
+    }
+
+    next(error);
+}
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
@@ -34,7 +44,7 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
             if (note) {
@@ -44,8 +54,7 @@ app.get('/api/notes/:id', (request, response) => {
             }
         })
         .catch(error => {
-            console.log(error);
-            response.status(400).send({ error: 'malformed id' });
+            next(error);
         })
 })
 
@@ -69,14 +78,31 @@ app.post('/api/notes', (request, response) => {
 
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 
-    response.status(204).end();
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body;
+
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote);
+        }).catch(error => next(error))
 })
 
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
