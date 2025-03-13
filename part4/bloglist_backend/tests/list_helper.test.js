fe41bef0173d0edd61497fc5f852e1f529268fd9
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-13 13:10:40 -0600

    refactor: modify blog list test helper and config

--- part4/bloglist_backend/tests/list_helper.test.js
+++ part4/bloglist_backend/tests/list_helper.test.js
@@ -9,38 +9,38 @@ test('Dummy returns one', () => {
   assert.strictEqual(result, 1);
 });
 
-const blogs = [
+const initialBlogs = [
   {
-    'title': 'Blog1',
-    'author': 'John',
-    'url': 'https://example.com/',
-    'likes': 13,
-    'id': '1'
+    title: 'Blog1',
+    author: 'John',
+    url: 'https://example.com/',
+    likes: 13,
+    id: '1'
   },
   {
-    'title': 'Blog2',
-    'author': 'Jane',
-    'url': 'https://example2.com/',
-    'likes': 2,
-    'id': '2'
+    title: 'Blog2',
+    author: 'Jane',
+    url: 'https://example2.com/',
+    likes: 2,
+    id: '2'
   },
   {
-    'title': 'Blog3',
-    'author': 'Guy',
-    'url': 'https://example3.com/',
-    'likes': 4,
-    'id': '3'
+    title: 'Blog3',
+    author: 'Guy',
+    url: 'https://example3.com/',
+    likes: 4,
+    id: '3'
   },
   {
-    'title': 'Blog4',
-    'author': 'Jane',
-    'url': 'https://example2.com/new',
-    'likes': 6,
-    'id': '4'
+    title: 'Blog4',
+    author: 'Jane',
+    url: 'https://example2.com/new',
+    likes: 6,
+    id: '4'
   }
 ];
 
-const oneBlogList = Array.of(blogs[1]);
+const oneBlogList = Array.of(initialBlogs[1]);
 
 const favorite = {
   'title': 'Blog1',
@@ -70,22 +70,26 @@ describe('Total likes', () => {
   });
 
   test('of a bigger list is calculated right', () => {
-    assert.strictEqual(listHelper.totalLikes(blogs), 25);
+    assert.strictEqual(listHelper.totalLikes(initialBlogs), 25);
   });
 });
 
 describe('Blog', () => {
   test('with the most likes found', () => {
-    assert.deepStrictEqual(listHelper.favoriteBlog(blogs), favorite);
+    assert.deepStrictEqual(listHelper.favoriteBlog(initialBlogs), favorite);
   });
 });
 
 describe('Author', () => {
   test('with the most blogs found', () => {
-    assert.deepStrictEqual(listHelper.mostBlogs(blogs), authorWithMostBlogs);
+    assert.deepStrictEqual(listHelper.mostBlogs(initialBlogs), authorWithMostBlogs);
   });
 
   test('with the most liked blogs', () => {
-    assert.deepStrictEqual(listHelper.mostLikes(blogs), authorWithMostLikes);
+    assert.deepStrictEqual(listHelper.mostLikes(initialBlogs), authorWithMostLikes);
   });
 });
+
+module.exports = {
+  initialBlogs
+};
