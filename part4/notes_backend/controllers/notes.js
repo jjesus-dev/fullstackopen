03dbb2f8b20fcfca9cc73380b49da2b8e4c2417b
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-12 22:01:05 -0600

    fix: modify error handling for notes `POST` method

--- part4/notes_backend/controllers/notes.js
+++ part4/notes_backend/controllers/notes.js
@@ -17,7 +17,7 @@ notesRouter.get('/:id', (request, response, next) => {
     });
 });
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
   const body = request.body;
 
   const note = new Note({
@@ -25,9 +25,12 @@ notesRouter.post('/', (request, response, next) => {
     important: body.important || false,
   });
 
-  note.save().then(savedNote => {
+  try {
+    const savedNote = await note.save();
     response.status(201).json(savedNote);
-  }).catch(error => next(error));
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 notesRouter.delete('/:id', (request, response, next) => {
