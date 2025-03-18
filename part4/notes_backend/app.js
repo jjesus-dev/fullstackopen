5be6cb87102dff5b130c92c3d6aed299549cbd62
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-17 22:39:46 -0600

    feat: add users-notes relationship & api test

--- part4/notes_backend/app.js
+++ part4/notes_backend/app.js
@@ -6,6 +6,7 @@ require('express-async-errors');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const notesRouter = require('./controllers/notes');
+const usersRouter = require('./controllers/users');
 const app = express();
 
 mongoose.set('strictQuery', false);
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
