8685c26694bdc9e5b344d9234c924d61bfdb0610
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-20 15:54:15 -0600

    feat: add blog list's users api for exercise 4.15
    feat: add `bcryptjs` package to hash passwords
    feat: add blogs' users model for database interaction
    feat: add users `GET` & `POST` web methods in blog list api

--- part4/bloglist_backend/app.js
+++ part4/bloglist_backend/app.js
@@ -3,6 +3,7 @@ const logger = require('./utils/logger');
 const express = require('express');
 require('express-async-errors');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -22,5 +23,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
