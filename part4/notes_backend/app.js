e49844ebbda7a92ed738ed90c1fa2c1cc9154f59
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-20 13:04:20 -0600

    feat: add login to notes api for authentication

--- part4/notes_backend/app.js
+++ part4/notes_backend/app.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const notesRouter = require('./controllers/notes');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const app = express();
 
 mongoose.set('strictQuery', false);
@@ -28,6 +29,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
