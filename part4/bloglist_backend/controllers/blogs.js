1fa6ec45c8d84b320da034f8d1d074819b8c2a81
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-21 13:45:12 -0600

    refactor: add `DELETE` blog auth for exercise 4.21

--- part4/bloglist_backend/controllers/blogs.js
+++ part4/bloglist_backend/controllers/blogs.js
@@ -42,8 +42,22 @@ blogsRouter.post('/', async (request, response) => {
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id);
-  response.status(204).end();
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'Invalid token' });
+  }
+
+  const user = await User.findById(decodedToken.id);
+  const blog = await Blog.findById(request.params.id);
+
+  if (blog.user.toString() === user._id.toString()) {
+    await Blog.findByIdAndDelete(request.params.id);
+    return response.status(204).end();
+  } else {
+    return response.status(401).json({ error: 'Invalid user' });
+  }
+
 });
 
 blogsRouter.put('/:id', async (request, response) => {
