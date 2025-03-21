78070e741bec9c2c4d47ce42b54e08f7e0bfc4ff
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-21 10:23:25 -0600

    feat: add blogs token-based auth for exercise 4.18

--- part4/bloglist_backend/app.js
+++ part4/bloglist_backend/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 require('express-async-errors');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -25,6 +26,7 @@ app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
