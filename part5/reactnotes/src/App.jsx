4499ce5ccf4d8c1d576f5a4f29789981b2276050
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-21 22:35:47 -0600

    feat: add notes login form & service for part5

--- part5/reactnotes/src/App.jsx
+++ part5/reactnotes/src/App.jsx
@@ -4,6 +4,7 @@ import Note from "./components/Note";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
 import noteService from "./services/notes";
+import loginService from "./services/login";
 
 import './index.css';
 
@@ -12,6 +13,9 @@ const App = () => {
   const [newNote, setNewNote] = useState('a new note...');
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   const hook = () => {
     noteService.getAll()
@@ -60,11 +64,49 @@ const App = () => {
       })
   }
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password
+      });
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      setErrorMessage(`Wrong credentials - ${exception.message}`)
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000);
+    }
+    console.log('logging in with', username);
+  }
+
   return (
     <>
       <div>
         <h1>Notes</h1>
         <Notification message={errorMessage} />
+        
+        <form onSubmit={handleLogin}>
+          <div>
+            username <input type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)} />
+          </div>
+          <div>
+            password <input type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)} />
+          </div>
+
+          <button type="submit">Login</button>
+        </form>
+        <br />
+
         <div>
           <button onClick={() => setShowAll(!showAll)}>
             Show {showAll ? 'important' : 'all'}
