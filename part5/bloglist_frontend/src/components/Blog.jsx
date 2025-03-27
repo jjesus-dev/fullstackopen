5f04bd0efd0d633765ebbd2e0146b7151b5ceb7a
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-26 23:33:32 -0600

    refactor: modify `Blog` component for exercise 5.7
    add view/hide button for blogs' data visibility
    add `like` button (pending functionality)

--- part5/bloglist_frontend/src/components/Blog.jsx
+++ part5/bloglist_frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [infoVisibility, setInfoVisibility] = useState(false);
+
+  const blogStyle = {
+    margin: 6,
+    padding: 2,
+    border: '2px solid'
+  };
+
+  const blogTitle = {
+    backgroundColor: '#ddffdd',
+    padding: 2
+  }
+
+  const toggleVisibility = () => {
+    setInfoVisibility(!infoVisibility);
+  }
+
+  const blogInformation = () => (
+    <div>
+      <p>Url: {blog.url}</p>
+      <p>Likes: {blog.likes} &nbsp; <button>like</button></p>
+      <p>Author: {blog.author}</p>
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      <p style={blogTitle}>
+        {blog.title} &nbsp;
+      <button onClick={() => toggleVisibility()}>
+        {infoVisibility ? 'Hide' : 'View'}
+      </button>
+      </p>
+
+      {infoVisibility && blogInformation()} 
+    </div>
+  );
+}
 
 export default Blog
