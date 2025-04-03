5d910ca136e27ab022bfae865d93bb995e8f8352
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-03 17:12:46 -0600

    feat: add blog's like button test for exercise 5.15
    add a test that checks if the like button is pressed and the blog component received an event as a prop
    refactor: modify previous test for simplicity

--- part5/bloglist_frontend/src/components/Blog.test.jsx
+++ part5/bloglist_frontend/src/components/Blog.test.jsx
@@ -2,41 +2,55 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
-test('renders content', () => {
-  const blog = {
-    author: 'John Doe',
-    title: 'How to test React apps'
-  };
+describe('<Blog />', () => {
+  let container;
+  let user;
+  const mockHandler = vi.fn();
 
-  const { container } = render(<Blog blog={blog} />);
+  beforeEach(() => {
+    const blog = {
+      author: 'John Doe',
+      title: 'How to test React apps',
+      likes: 10,
+      url: 'http://example.com/howtotest',
+      user: ''
+    };
 
-  const div = container.querySelector('.blog');
-  expect(div).toHaveTextContent('How to test React apps');
-  expect(div).toHaveTextContent('John Doe');
+    user = userEvent.setup();
 
-  //screen.debug(div);
-});
+    container = render(
+      <Blog blog={blog} updateBlog={mockHandler} />
+    ).container;
+  })
 
-test('renders likes & url after clicking the button', async () => {
-  const blog = {
-    author: 'John Doe',
-    title: 'How to test React apps',
-    likes: 10,
-    url: 'http://example.com/howtotest'
-  };
-
-  const user = userEvent.setup();
-
-  const { container } = render(
-    <Blog blog={blog} />
-  );
-
-  const button = screen.getByText('View');
-  await user.click(button);
+  test('renders default content', () => {
+    const div = container.querySelector('.blog');
+    expect(div).toHaveTextContent('How to test React apps');
+    expect(div).toHaveTextContent('John Doe');
   
-  const div = container.querySelector('.blog');
-  expect(div).toHaveTextContent('10');
-  expect(div).toHaveTextContent('http://example.com/howtotest');
+    //screen.debug(div);
+  });
 
+  test('renders likes & url after clicking the button', async () => {
+    const button = screen.getByText('View');
+    await user.click(button);
+    
+    const div = container.querySelector('.blog');
+    expect(div).toHaveTextContent('10');
+    expect(div).toHaveTextContent('http://example.com/howtotest');
+  })
 
+  test('likes a blog 2 times, after showing the full info', async () => {
+    const button = screen.getByText('View');
+    await user.click(button);
+
+    const likeButton = screen.getByText('like');
+    await user.click(likeButton);
+    
+    const div = container.querySelector('.blog');
+    expect(mockHandler.mock.calls).toHaveLength(1);
+
+    await user.click(likeButton);
+    expect(mockHandler.mock.calls).toHaveLength(2);
+  })
 })
