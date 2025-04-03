f49724cbcdbe0fb20a8fcf609edeb2afac380565
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-03 15:41:01 -0600

    feat: add blog's info button test for exercise 5.14
    feat: add `user-event` library to test clicking/pressing buttons

--- part5/bloglist_frontend/src/components/Blog.test.jsx
+++ part5/bloglist_frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 test('renders content', () => {
@@ -15,3 +16,27 @@ test('renders content', () => {
 
   //screen.debug(div);
 });
+
+test('renders likes & url after clicking the button', async () => {
+  const blog = {
+    author: 'John Doe',
+    title: 'How to test React apps',
+    likes: 10,
+    url: 'http://example.com/howtotest'
+  };
+
+  const user = userEvent.setup();
+
+  const { container } = render(
+    <Blog blog={blog} />
+  );
+
+  const button = screen.getByText('View');
+  await user.click(button);
+  
+  const div = container.querySelector('.blog');
+  expect(div).toHaveTextContent('10');
+  expect(div).toHaveTextContent('http://example.com/howtotest');
+
+
+})
