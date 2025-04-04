707f6a15421a82b2294213b8199c5d4f834a2da0
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-03 22:30:33 -0600

    feat: add new note form e2e test after login
    add a test for notes creation after user logs in
    modify default page url to use an env variable

--- part5/notes_e2e/tests/note_app.spec.js
+++ part5/notes_e2e/tests/note_app.spec.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 describe('Note app', () => {
   beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+    await page.goto(process.env.TESTS_BASEURL)
   })
 
   test('front page can be opened', async ({ page }) => {
@@ -12,12 +12,29 @@ describe('Note app', () => {
     await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
   })
 
-  test('login form can be opened', async ({ page }) => {
+  test('user can log in', async ({ page }) => {
     await page.getByRole('button', { name: 'Login' }).click()
     await page.getByTestId('txtUsername').fill(process.env.TESTS_USERNAME)
     await page.getByTestId('txtPassword').fill(process.env.TESTS_USERPASS)
     await page.getByRole('button', { name: 'Login' }).click()
 
     await expect(page.getByText('Testing User logged-in')).toBeVisible()
+  })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'Login' }).click()
+      await page.getByTestId('txtUsername').fill(process.env.TESTS_USERNAME)
+      await page.getByTestId('txtPassword').fill(process.env.TESTS_USERPASS)
+      await page.getByRole('button', { name: 'Login' }).click()
+    })
+
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'New note' }).click()
+      await page.getByRole('textbox').fill('A new note created using Playwright')
+      await page.getByRole('button', { name: 'Save' }).click()
+
+      await expect(page.getByText('A new note created using Playwright')).toBeVisible()
+    })
   })
 })
