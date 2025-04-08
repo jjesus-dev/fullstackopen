491e5f38239792564bdc7ed7a58a84a1a163a47a
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-08 15:52:15 -0600

    feat: add e2e login form tests for exercise 5.18
    feat: add `dotenv` packages to load env variables
    feat: add test timeout & base url parameters in playwright's config

--- part5/bloglist_e2e/tests/bloglist.spec.js
+++ part5/bloglist_e2e/tests/bloglist.spec.js
@@ -1,11 +1,39 @@
+import "dotenv/config";
 import test, { expect, beforeEach, describe } from "@playwright/test";
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Testing User',
+        username: process.env.TESTS_USERNAME,
+        password: process.env.TESTS_PASSWORD
+      }
+    })
+
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('txtUsername').fill(process.env.TESTS_USERNAME)
+      await page.getByTestId('txtPassword').fill(process.env.TESTS_PASSWORD)
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('Testing User successfully logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('txtUsername').fill(process.env.TESTS_USERNAME)
+      await page.getByTestId('txtPassword').fill('wrongString')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('Wrong credentials')).toBeVisible()
+    })
   })
 })
