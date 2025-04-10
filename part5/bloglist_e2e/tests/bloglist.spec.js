9b569034e83fee2d3c1f75b5e2a537440c5db7d0
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-09 19:14:56 -0600

    feat: add delete blog e2e test for exercise 5.21

--- part5/bloglist_e2e/tests/bloglist.spec.js
+++ part5/bloglist_e2e/tests/bloglist.spec.js
@@ -2,6 +2,9 @@ import "dotenv/config";
 import test, { expect, beforeEach, describe } from "@playwright/test";
 
 describe('Blog app', () => {
+  const otherUsername = `${process.env.TESTS_USERNAME}New`
+  const otherPassword = `${process.env.TESTS_PASSWORD}SOm3`
+
   beforeEach(async ({ page, request }) => {
     await request.post('/api/testing/reset')
     await request.post('/api/users', {
@@ -9,6 +12,14 @@ describe('Blog app', () => {
         name: 'Testing User',
         username: process.env.TESTS_USERNAME,
         password: process.env.TESTS_PASSWORD
+      }
+    })
+
+    await request.post('/api/users', {
+      data: {
+        name: 'Testing User #2',
+        username: otherUsername,
+        password: otherPassword
       }
     })
 
@@ -75,5 +86,44 @@ describe('Blog app', () => {
       await expect(likeElement).toContainText('Likes: 1')
       await expect(page.getByText('Testing User liked an entry')).toBeVisible()
     })
+  })
+
+  describe('When trying to delete a blog', () => {
+    beforeEach(async ({ page, request }) => {
+      await page.getByTestId('txtUsername').fill(process.env.TESTS_USERNAME)
+      await page.getByTestId('txtPassword').fill(process.env.TESTS_PASSWORD)
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('Testing User successfully logged in')).toBeVisible()
+
+      await page.getByRole('button', { name: 'Create new blog' }).click()
+      await page.getByTestId('txtTitle').fill('How to test frontend apps?')
+      await page.getByTestId('txtAuthor').fill('John Doe')
+      await page.getByTestId('txtUrl').fill('http://example.com/testingReact')
+      await page.getByRole('button', { name: 'Create' }).click()
+
+      await expect(page.getByText('Testing User added a new entry:')).toBeVisible()
+      await page.getByRole('button', { name: 'Logout' }).click()
+    })
+
+    test('a blog is deleted by its owner', async ({ page }) => {
+      await page.getByTestId('txtUsername').fill(process.env.TESTS_USERNAME)
+      await page.getByTestId('txtPassword').fill(process.env.TESTS_PASSWORD)
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('Testing User successfully logged in')).toBeVisible()
+
+      await page.getByText('How to test frontend apps?').waitFor()
+      await page.getByRole('button', { name: 'View' }).click()
+      
+      page.on('dialog', async dialog => {
+        await dialog.accept()
+          .catch(error => console.log(error.message));
+      });
+      await page.getByRole('button', { name: 'Remove' }).click()
+      
+      await expect(page.getByText('Testing User has removed an entry')).toBeVisible()
+    })
+
   })
 })
