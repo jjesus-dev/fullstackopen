35a298d3056038287e47c65e87b4e79e98871e06
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-10 21:38:58 -0600

    refactor: modify app using Redux for state management

--- part6/reduxnotes/src/App.jsx
+++ part6/reduxnotes/src/App.jsx
@@ -1,20 +1,45 @@
-import { useState } from 'react'
+import { createStore } from 'redux'
 
-function App() {
-  const [count, setCount] = useState(0)
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    state.push(action.payload)
+    return state
+  }
 
+  return state
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with action',
+    important: false,
+    id: 2
+  }
+})
+
+const App = () => {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
+    <div>
+      <ul>
+        {store.getState().map(note => 
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+      </ul>
+    </div>
   )
 }
 
