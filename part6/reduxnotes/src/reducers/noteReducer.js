0c8dfb9ccb568d18009a60a3b14afd665789a50b
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-12 16:57:15 -0600

    refactor: modify app to use notes components
    feat: add `NewNote` component (presentational) to handle new notes form
    feat: add `Notes` component (container) to handle the notes list and toggle importance

--- part6/reduxnotes/src/reducers/noteReducer.js
+++ part6/reduxnotes/src/reducers/noteReducer.js
@@ -19,7 +19,7 @@ const noteReducer = (state = [], action) => {
 }
 
 const generateId = () => {
-  Number((Math.random() * 1000000).toFixed(0))
+  return Number((Math.random() * 1000000).toFixed(0))
 }
 
 export const createNote = (content) => {
