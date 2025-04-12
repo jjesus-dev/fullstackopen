162688a90b657bd8aa690f21542d6407846b24bc
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-12 16:34:08 -0600

    refactor: modify notes components to use react-redux
    feat: add `react-redux` package
    feat: add `react-redux` provider to pass the state store as props
    refactor: create app file to manage notes frontend
    refactor: add notes & ids creation to `noteReducer`

---
+++ part6/reduxnotes/src/App.jsx
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from "react-redux"
+import { createNote, toggleImportanceOf } from "./reducers/noteReducer"
+
+const App = () => {
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+
+    dispatch(createNote(content))
+  }
+
+  const toggleImportance = (id) => {
+    dispatch(toggleImportanceOf(id))
+  }
+
+  return (
+    <div>
+      <form onSubmit={addNote}>
+        <input type='text' name='note' />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {notes.map(note => 
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
--- part6/reduxnotes/src/main.jsx
+++ part6/reduxnotes/src/main.jsx
@@ -1,96 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
-const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    state.concat(action.payload)
-    return state
-  }
-
-  return state
-}
+import App from './App'
+import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with action',
-    important: false,
-    id: 2
-  }
-})
-
-const generateId = () => {
-  Number((Math.random() * 1000000).toFixed(0))
-}
-
-const App = () => {
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-
-    store.dispatch(createNote(content))
-  }
-
-  const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id))
-  }
-
-  const createNote = (content) => {
-    return {
-      type: 'NEW_NOTE',
-      payload: {
-        content,
-        important: false,
-        id: generateId()
-      }
-    }
-  }
-
-  const toggleImportanceOf = (id) => {
-    return {
-      type: 'TOGGLE_IMPORTANCE',
-      payload: { id }
-    }
-  }
-
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input type='text' name='note' />
-        <button type="submit">Add</button>
-      </form>
-      <ul>
-        {store.getState().map(note => 
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
-    </div>
-  )
-}
-
-const root = createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(
-      <App />
-  )
-}
-
-renderApp()
-store.subscribe(renderApp)
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
