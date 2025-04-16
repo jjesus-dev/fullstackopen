91d5216940d75cb47bbda4140904b54c0830417f
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-16 17:56:37 -0600

    feat: add anecdotes service for exercise 6.14
    feat: add `axios` & `json-server` packages
    feat: add local db with initial anecdotes
    refactor: remove initial anecdotes from reducer to
    refactor: modify app to fetch anecdotes from `json-server` at start

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -1,29 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
       const content = action.payload;
@@ -38,7 +19,7 @@ const anecdoteSlice = createSlice({
       const anecdoteToVote = state.find(a => a.id === id);
       const votedAnecdote = {
         ...anecdoteToVote,
-        votes: anecdoteToVote.votes+1
+        votes: anecdoteToVote.votes + 1
       };
 
       const newAnecdotes = state.map(anecdote => 
@@ -46,9 +27,12 @@ const anecdoteSlice = createSlice({
       );
 
       return newAnecdotes.sort((a, b) => b.votes - a.votes)
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions
+export const { createAnecdote, voteAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
