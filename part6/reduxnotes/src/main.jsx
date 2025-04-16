71c0b400ccd1f0a4fcebafcd98805798d5e364fc
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-15 20:57:51 -0600

    refactor: modify reducers & store with Redux Toolkit
    modify note reducer and tests implementing Redux Toolkit
    feat: add Redux Toolkit package

--- part6/reduxnotes/src/main.jsx
+++ part6/reduxnotes/src/main.jsx
@@ -1,18 +1,17 @@
 import { createRoot } from 'react-dom/client'
-
 import { Provider } from 'react-redux'
-import { combineReducers, createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
 import noteReducer, { createNote } from './reducers/noteReducer'
 import filterReducer, { filterChange } from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
