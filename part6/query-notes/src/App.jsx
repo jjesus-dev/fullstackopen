9f88540c27047b449806965f676aec597af744f6
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-17 11:23:41 -0600

    refactor: modify app to manage data with react-query

--- part6/query-notes/src/App.jsx
+++ part6/query-notes/src/App.jsx
@@ -1,19 +1,46 @@
-import { useState } from 'react'
+import { useQuery } from "@tanstack/react-query"
+import { getNotes } from "./request"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const addNote = async (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    console.log(content);
+    
+  }
+
+  const toggleImportance = (note) => {
+    console.log('toggle importance of', note.id);
+    
+  }
+
+  const result = useQuery({
+    queryKey: ['notes'],
+    queryFn: getNotes
+  })
+  console.log(JSON.parse(JSON.stringify(result)));
+  
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const notes = result.data
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input type='text' name='note' />
+        <button type='submit'>Add</button>
+      </form>
+      <ul>
+        {notes.map(note =>
+          <li key={note.id} onClick={() => toggleImportance(note)}>
+            {note.content}
+          </li>
+        )}
+      </ul>
     </>
   )
 }
---
+++ part6/query-notes/src/request.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const getNotes = () => {
+  return axios.get('http://localhost:3001/notes')
+    .then(res => res.data)
+}
