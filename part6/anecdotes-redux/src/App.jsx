9924ab16f086c748b3b4a988b0944cff0ad1f3af
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-16 20:56:50 -0600

    feat: add anecdotes initializer for exercise 6.16

--- part6/anecdotes-redux/src/App.jsx
+++ part6/anecdotes-redux/src/App.jsx
@@ -5,15 +5,12 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteFilter from './components/AnedcoteFilter'
 import Notification from './components/Notification'
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 function App() {
   const dispatch = useDispatch()
   useEffect(() => {
-    anecdoteService.getAll().then(
-      anecdotes => dispatch(setAnecdotes(anecdotes))
-    )
+    dispatch(initializeAnecdotes())
   }, [])
 
   return (
