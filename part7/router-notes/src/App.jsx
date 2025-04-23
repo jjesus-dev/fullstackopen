d7dcead47c274e006904dc91d11f0f3672cea62f
Javier J. Martínez M. <jj_dev@outlook.com>
2025-04-22 20:18:52 -0600

    refactor: modify notes app to show login & notes info
    feat: add `Note` & `Login` components

--- part7/router-notes/src/App.jsx
+++ part7/router-notes/src/App.jsx
@@ -1,9 +1,39 @@
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './components/Home'
+import Note from './components/Note'
 import Notes from './components/Notes'
 import Users from './components/Users'
+import Login from './components/Login'
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas'
+    }
+  ])
+
+  const [user, setUser] = useState(null)
+
+  const login = (user) => {
+    setUser(user)
+  }
+
   const padding = {
     padding: '5px'
   }
@@ -16,16 +46,26 @@ function App() {
         <Link style={padding} to='/'>Home</Link>
         <Link style={padding} to='/notes'>Notes</Link>
         <Link style={padding} to='/users'>Users</Link>
+        {user
+          ? <em>{user} logger in</em>
+          : <Link style={padding} to='/login'>Login</Link>
+        }
       </div>
 
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/notes' element={<Notes />} />
-        <Route path='/users' element={<Users />} />
+        <Route path='/notes/:id' element={<Note notes={notes} />} />
+        <Route path='/notes' element={<Notes notes={notes} />} />
+        <Route path='/users' element={user 
+          ? <Users />
+          : <Navigate replace to='/login' />}
+        />
+        <Route path='/login' element={<Login onLogin={login} />} />
       </Routes>
 
       <div>
-        <i>Note app, Department of Computer Science 2025</i>
+        <br />
+        <em>Note app, Department of Computer Science 2025</em>
       </div>
     </Router>
     </>
--- part7/router-notes/src/components/Users.jsx
+++ part7/router-notes/src/components/Users.jsx
@@ -1,7 +1,12 @@
 const Users = () => {
   return (
     <div>
-      <h2>Users</h2>
+      <h2>TKTL notes app</h2>
+      <ul>
+        <li>Matti Luukkainen</li>
+        <li>Juha Tauriainen</li>
+        <li>Arto Hellas</li>
+      </ul>
     </div>
   )
 }
