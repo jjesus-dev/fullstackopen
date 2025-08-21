5b6c8215c4f27d16de19af684857530566ad3be2
Javier J. Martínez M. <jj_dev@outlook.com>
2025-08-20 18:23:55 -0600

    feat: add backend & frontend for part7 exercises
    Add bloglist backend & frontend codebase to perform part7 exercises (7.9-7.21)

---
+++ part7/bloglist-app/frontend/eslint.config.js
@@ -0,0 +1,45 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import stylisticJs from '@stylistic/eslint-plugin-js';
+
+export default [
+  { ignores: ['dist', '.eslintrc.cjs'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      '@stylistic/js': stylisticJs,
+      'vitest-globals/env': true
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'eqeqeq': 'error',
+      'no-console': 'error',
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'always', { 'omitLastInOneLineClassBody': true }],
+      '@stylistic/js/arrow-spacing': 'error',
+      '@stylistic/js/object-curly-spacing': ['error', 'always'],
+      '@stylistic/js/no-trailing-spaces': 'error',
+    },
+  },
+];
