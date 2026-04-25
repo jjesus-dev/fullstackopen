88bbe4735342ee5e24061919cfb8ccb27c44f3e8
Javier J. Martínez M. <jj_dev@outlook.com>
2026-04-25 00:22:33 -0600

    feat: add `useAnecdotes` hook for exercise 7.4
    + Add hook that fetches anecdotes from the backend.

--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const useField = (type) => {
   const [value, setValue] = useState('')
@@ -16,5 +16,23 @@ export const useField = (type) => {
     value,
     onChange,
     onReset
+  }
+}
+
+export const useAnecdotes = () => {
+  const baseUrl = 'http://localhost:3001/anecdotes'
+  const [anecdotes, setAnecdotes] = useState([])
+
+  useEffect(() => {
+    fetch(baseUrl)
+      .then(response => response.json())
+      .then(json => {
+        setAnecdotes(json)
+      })
+      .catch(error => console.error('Failed to fetch notes:', error))
+  })
+
+  return {
+    anecdotes
   }
 }
